92956995d455bbf8b969196c6273facf76a33909
Oleksandr Rymar <twitchsqien@gmail.com>
2023-03-06 22:24:37 +0100

    database connect

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello1')
+import pymysql.cursors
+
+
+connection = pymysql.connect(host = 'localhost',
+                             port = 3306,
+                             user = 'root',
+                             password = '',
+                             database = 'todolist',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+connection.commit()
+
+with connection.cursor() as cursor:
+    sql = "SELECT * FROM tasks"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
